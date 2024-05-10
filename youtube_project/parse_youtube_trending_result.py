c6db95a532075302e2fe9f5478c917a56ecea6aa
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2024-05-10 14:39:55 +0200

    Added Pydantic Models for Parsing

--- youtube_project/parse_youtube_trending_result.py
+++ youtube_project/parse_youtube_trending_result.py
@@ -1,23 +1,32 @@
-from youtube_project.youtube_api  import extract_trending_youtube_music_videos
-import pandas as pd
-from datetime import datetime, time
-import re
-import numpy as np
 import os
+import re
+import json
+from datetime import date, datetime, time
 
-dag_path = os.getcwd()
+import pandas as pd
+from pydantic import BaseModel, Field, computed_field, field_validator
 
-def generate_time_duration(time_string):
+from youtube_api import extract_trending_youtube_videos
+
+
+def generate_time_duration(time_string: str) -> time:
     """
     Method for transforming the time raw string 'PT3M36S' into a datetime object
+    Result: 00:03:36
     """
-    hour_pattern = re.compile(r'(\d+)H')
-    minute_pattern = re.compile(r'(\d+)M')
-    second_pattern = re.compile(r'(\d+)S')
+    hour_pattern = re.compile(r"(\d+)H")
+    minute_pattern = re.compile(r"(\d+)M")
+    second_pattern = re.compile(r"(\d+)S")
 
-    hour_match = hour_pattern.search(time_string)      # hour_pattern.search(time_string).group(1)/re.search(hour_pattern, time_string).group(1)
-    minute_match = minute_pattern.search(time_string)  # minute_pattern.search(time_string).group(1) /  re.search(minute_pattern, time_string).group(1)
-    second_match = second_pattern.search(time_string)  # second_pattern.search(time_string).group(1) / re.search(second_pattern, time_string).group(1)
+    hour_match = hour_pattern.search(
+        time_string
+    )  # hour_pattern.search(time_string).group(1)/re.search(hour_pattern, time_string).group(1)
+    minute_match = minute_pattern.search(
+        time_string
+    )  # minute_pattern.search(time_string).group(1) /  re.search(minute_pattern, time_string).group(1)
+    second_match = second_pattern.search(
+        time_string
+    )  # second_pattern.search(time_string).group(1) / re.search(second_pattern, time_string).group(1)
 
     hours = int(hour_match.group(1)) if hour_match else 0
     minutes = int(minute_match.group(1)) if minute_match else 0
@@ -25,32 +34,70 @@ def generate_time_duration(time_string):
 
     return time(hours, minutes, seconds)
 
-def parse_top_10_youtube_music_trending(youtube_trending_results_raw):
+
+class VideoMetaData(BaseModel):
+    song_title: str = Field(alias="title")
+    published_date: datetime = Field(alias="publishedAt")
+    channel: str = Field(alias="channelTitle")
+
+
+class VideoContentDetails(BaseModel):
+    duration: time = Field(alias="duration")
+
+    @field_validator("duration", mode="before")
+    @classmethod
+    def get_video_duration(cls, duration: str) -> time:
+        """
+        Method for parsing the duration field
+        """
+        return generate_time_duration(duration)
+
+
+class VideoStatistics(BaseModel):
+    views: int = Field(alias="viewCount")
+
+
+class YoutubeData(BaseModel):
+    id: str = Field(repr=False)
+    snippet: VideoMetaData
+    contentDetails: VideoContentDetails
+    statistics: VideoStatistics
+
+    @computed_field(alias="video_url")
+    def video_url(self) -> str:
+        return f"https://youtu.be/{self.id}"
+
+
+dag_path = os.getcwd()
+
+
+def parse_top_10_youtube_music_trending(
+    youtube_trending_results_raw: json,
+) -> pd.DataFrame:
     """
     Method for parsing the top 10 music from raw json response
     """
-    youtube_trending_result_list = youtube_trending_results_raw['items']
+    results = [YoutubeData(**item) for item in youtube_trending_results_raw["items"]]
 
-    parsed_result_list = list()
-
-    for result in youtube_trending_result_list:
-
-        parsed_result_dict = {
-            'song_title': result['snippet']['title'].strip(),
-            'published_date': result['snippet']['publishedAt'],
-            'channel': result['snippet']['channelTitle'],
-            'duration': generate_time_duration(result['contentDetails']['duration']),
-            'views': int(result['statistics']['viewCount']),
-            'video_url': f"https://youtu.be/{result['id']}"
-        }
-        parsed_result_list.append(parsed_result_dict)
-
+    parsed_result_list = []
+    for result in results:
+        result_dict = result.model_dump()
+        parsed_result_list.append(
+            {
+                **result_dict["snippet"],
+                **result_dict["contentDetails"],
+                **result_dict["statistics"],
+                **{"video_url": result_dict["video_url"]},
+            }
+        )
 
     df = pd.DataFrame(parsed_result_list)
-    df.insert(0, 'datetime_retrieved', datetime.strftime(datetime.now(), '%d-%m-%Y_%H:%M'))
-    df.to_csv(f"{dag_path}/processed_data/youtube_trending_results_{date.today().strftime('%d-%m-%Y')}.csv", index=False)
+    df.to_csv(
+        f"{dag_path}/processed_data/youtube_trending_results_{date.today().strftime('%d-%m-%Y')}.csv",
+        index=False,
+    )
     return df
 
 
 if __name__ == "__main__":
-    parse_top_10_youtube_music_trending(extract_trending_youtube_music_videos())
+    parse_top_10_youtube_music_trending(extract_trending_youtube_videos())
