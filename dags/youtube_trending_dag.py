7ee720886f7a8e3a7f1c994c06bf323d65d3aa2d
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2024-05-19 16:32:54 +0200

    Added new task cp to intermediate bucket

--- dags/youtube_trending_dag.py
+++ dags/youtube_trending_dag.py
@@ -1,17 +1,16 @@
-
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash  import BashOperator
 from airflow.providers.http.sensors.http import HttpSensor
-from airflow.utils.dates import days_ago
-from parse_youtube_trending_results import parse_top_10_youtube_music_trending
+from parse_youtube_trending_result  import parse_top_10_youtube_music_trending
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor 
 from plotly_viz import create_HTML_dashboard
-
+import pendulum
 from youtube_api import extract_trending_youtube_videos
 
 default_args = {
     'owner': 'ramkumar',
-    'start_date': days_ago(0) ,#pendulum.datetime(2023,3,29,15,50,tz='Europe/Berlin'),#datetime(2023,3,29,13),# days_ago(0)
+    'start_date': pendulum.today('Europe/Berlin') ,# pendulum.datetime(2024,5,19,tz='Europe/Berlin'),#datetime(2023,3,29,13)
     'retries':1,
 
 }
@@ -43,11 +42,15 @@ with  DAG(
     
 )
 
-	task_3 = PythonOperator(
-    task_id='create_plotly_dashboard',
-    python_callable=create_HTML_dashboard,
-    op_kwargs = {'df': task_2.output},
-    
+	task_3 = BashOperator(
+    task_id='copy_data_from_landing_to_intermediate_bucket',
+    bash_command = 'aws s3 cp {{ti.xcom_pull("parse_of_videos")[1]}} s3://youtube-data-bucket-ram-intermediate-data/{{ti.xcom_pull("parse_of_videos")[0]}}'   
 )
+	
+# 	task_3 = PythonOperator(
+#     task_id='create_plotly_dashboard',
+#     python_callable=create_HTML_dashboard,
+#     op_kwargs = {'df': task_2.output} 
+# )
 
-	task_0 >> task_1 >> task_2 >> task_3
+	task_0 >> task_1 >> task_2  >> task_3
