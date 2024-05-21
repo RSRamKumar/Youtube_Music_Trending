d6af0cdd813526d2d6d936810c13b2ab1867d138
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2024-05-21 13:30:39 +0200

    Update plotly_visualization.py

--- youtube_project/plotly_visualization.py
+++ youtube_project/plotly_visualization.py
@@ -10,14 +10,13 @@ from dash import Dash
 from datetime import datetime, time, date
 
 
-dag_path = os.getcwd()
 
-
-def create_HTML_dashboard(df):
+def create_HTML_dashboard(file_path):
+    df = pd.read_csv(file_path)
     app = Dash(__name__)
-    scatter_figure =px.scatter(df, x="song_title", y="views", size="views" , color='channel' ,
+    scatter_figure =px.scatter(df, x="song_title", y="views_count", size="views_count" , color='channel' ,
                            custom_data=["video_url"], size_max=20, template='plotly_dark', facet_col='channel', facet_col_wrap=3,
-                           hover_name='channel', labels= {'channel': 'Top 10 Music Channels', 'views': 'Total Views',  },  
+                           hover_name='channel', labels= {'channel': 'Top 10 Music Channels', 'views_count': 'Total Views',  },  
                            title='Trending Musics in India'
                             )
     scatter_figure.update_layout(width=1200, height=500, title_x=0.5)
@@ -35,9 +34,12 @@ def create_HTML_dashboard(df):
         if clickData:
             webbrowser.open(clickData["points"][0]["customdata"][0])
 
-    scatter_figure.write_html(f"{dag_path}/processed_data/youtube_trending_results_dashboard_{date.today().strftime('%d-%m-%Y')}.html")
+    output_file_name = f"youtube_trending_results_dashboard_{date.today().strftime('%d-%m-%Y')}.html"
+    path = os.getcwd()
+    scatter_figure.write_html(f'{path}/{output_file_name}')
+    return f'{path}/{output_file_name}'
 
 
 
 if __name__ == "__main__":
-    create_HTML_dashboard(df)
+    create_HTML_dashboard(file_path)
