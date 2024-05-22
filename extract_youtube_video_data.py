5ec72a38535f9748e12d6f9f2ed068559049d218
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2024-05-22 18:27:16 +0200

    Added Extract Youtube Data Script

---
+++ extract_youtube_video_data.py
@@ -0,0 +1,31 @@
+import googleapiclient.discovery
+
+
+def extract_trending_youtube_videos_raw_data():
+    """
+    Method for extracting the Youtube Trending data
+    for the region India (IN) and
+    music category (10)
+    """
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = ''
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+    request = youtube.videos().list(
+        part="contentDetails,id,snippet,statistics",
+        chart="mostPopular",
+        maxResults=30,
+        regionCode="IN",
+        videoCategoryId="10"
+    )
+    response = request.execute()
+
+    return response
+
+
+if __name__ == "__main__":
+    extract_trending_youtube_videos_raw_data()
+     
