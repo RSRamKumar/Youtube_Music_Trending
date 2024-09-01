5c5069a3e4c220f63f9d013ff4c364c74fbecc5a
Ram Kumar R S <39699070+RSRamKumar@users.noreply.github.com>
2024-09-01 09:27:08 +0200

    Update parse_youtube_trending_result.py

--- youtube_project/parse_youtube_trending_result.py
+++ youtube_project/parse_youtube_trending_result.py
@@ -92,7 +92,6 @@ def parse_top_youtube_music_trending_for_the_day(
 ) -> List:
     """
     Method for parsing the top 10 music from raw json response
-    and write it to the file in the S3 bucket
     """
     results = [YoutubeData(**item) for item in youtube_trending_results_raw["items"]]
 
